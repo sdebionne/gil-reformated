b6c5a404cf459e37c1cf02882d3e328675821388
Lubomir Bourdev <lbourdev@adobe.com>
2007-04-23 23:15:01 +0000

    Initial submission of GIL 2.0
    [SVN r37494]

---
+++ include/boost/gil/gil_all.hpp
@@ -0,0 +1,41 @@
+/*
+    Copyright 2005-2007 Adobe Systems Incorporated
+    Distributed under the MIT License (see accompanying file LICENSE_1_0_0.txt
+    or a copy at http://opensource.adobe.com/licenses.html)
+*/
+
+/*************************************************************************************************/
+
+#ifndef GIL_ALL_HPP
+#define GIL_ALL_HPP
+
+////////////////////////////////////////////////////////////////////////////////////////
+/// \file
+/// \brief  Includes all GIL files for convenience
+/// \author Lubomir Bourdev and Hailin Jin \n
+///         Adobe Systems Incorporated
+///
+///
+////////////////////////////////////////////////////////////////////////////////////////
+
+#include "algorithm.hpp"
+#include "channel_algorithm.hpp"
+#include "color_convert.hpp"
+#include "device_n.hpp"
+#include "gil_config.hpp"
+#include "image.hpp"
+#include "image_view_factory.hpp"
+#include "iterator_from_2d.hpp"
+#include "metafunctions.hpp"
+#include "packed_pixel.hpp"
+#include "pixel.hpp"
+#include "pixel_iterator_adaptor.hpp"
+#include "planar_pixel_iterator.hpp"
+#include "planar_pixel_reference.hpp"
+#include "step_iterator.hpp"
+#include "typedefs.hpp"
+#include "virtual_locator.hpp"
+// Uncomment this line to help in porting your code from an older version of GIL
+//#include "deprecated.hpp"
+
+#endif
