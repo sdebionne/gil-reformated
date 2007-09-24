cc57b5b6c443234331de43f15fc48b6f1b423227
Hailin Jin <hljin@adobe.com>
2007-09-24 18:25:48 +0000

    GIL: fixed endian-ness related bugs for bit_aligned_pixel_reference and pixel_aligned_pixel_iterator
    [SVN r39511]

--- include/boost/gil/bit_aligned_pixel_iterator.hpp
+++ include/boost/gil/bit_aligned_pixel_iterator.hpp
@@ -212,26 +212,27 @@ struct dynamic_x_step_type<
 //  iterator_type_from_pixel
 /////////////////////////////
 
-template <typename C, typename L, bool M>
-struct iterator_type_from_pixel<const bit_aligned_pixel_reference<C, L, M>,
+template <typename B, typename C, typename L, bool M>
+struct iterator_type_from_pixel<const bit_aligned_pixel_reference<B, C, L, M>,
                                 false, false, false> {
-  typedef bit_aligned_pixel_iterator<bit_aligned_pixel_reference<C, L, false>>
+  typedef bit_aligned_pixel_iterator<
+      bit_aligned_pixel_reference<B, C, L, false>>
       type;
 };
 
-template <typename C, typename L, bool M>
-struct iterator_type_from_pixel<const bit_aligned_pixel_reference<C, L, M>,
+template <typename B, typename C, typename L, bool M>
+struct iterator_type_from_pixel<const bit_aligned_pixel_reference<B, C, L, M>,
                                 false, false, true> {
-  typedef bit_aligned_pixel_iterator<bit_aligned_pixel_reference<C, L, true>>
+  typedef bit_aligned_pixel_iterator<bit_aligned_pixel_reference<B, C, L, true>>
       type;
 };
 
-template <typename C, typename L, bool M, bool IsPlanar, bool IsStep,
-          bool IsMutable>
-struct iterator_type_from_pixel<bit_aligned_pixel_reference<C, L, M>, IsPlanar,
-                                IsStep, IsMutable>
+template <typename B, typename C, typename L, bool M, bool IsPlanar,
+          bool IsStep, bool IsMutable>
+struct iterator_type_from_pixel<bit_aligned_pixel_reference<B, C, L, M>,
+                                IsPlanar, IsStep, IsMutable>
     : public iterator_type_from_pixel<
-          const bit_aligned_pixel_reference<C, L, M>, IsPlanar, IsStep,
+          const bit_aligned_pixel_reference<B, C, L, M>, IsPlanar, IsStep,
           IsMutable> {};
 
 } // namespace gil
