4bcbd96b9300d93921bd465c7237251232b064e7
Christian Henning <chhenning@gmail.com>
2013-04-29 00:14:16 +0000

    Renamed test suite.
    [SVN r84080]

--- toolbox/test/indexed_image_test.cpp
+++ toolbox/test/indexed_image_test.cpp
@@ -16,7 +16,7 @@ using namespace std;
 using namespace boost;
 using namespace gil;
 
-BOOST_AUTO_TEST_SUITE(index_image_test_suite)
+BOOST_AUTO_TEST_SUITE(toolbox_tests)
 
 BOOST_AUTO_TEST_CASE(index_image_test) {
   {
