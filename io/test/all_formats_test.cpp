53fb8e73455f62f22b10b53d27823144a5706e16
Stefan Seefeld <stefan@seefeld.name>
2017-11-16 19:11:46 -0500

    Perform actual I/O during testing.

--- io/test/all_formats_test.cpp
+++ io/test/all_formats_test.cpp
@@ -16,12 +16,13 @@
 #include <boost/gil/extension/io/tiff_all.hpp>
 
 #define BOOST_TEST_MODULE all_formats_test
-#include <boost/test/unit_test.hpp>
-
 #include "paths.hpp"
+#include <boost/filesystem.hpp>
+#include <boost/test/unit_test.hpp>
 
 using namespace std;
 using namespace boost::gil;
+namespace fs = boost::filesystem;
 
 BOOST_AUTO_TEST_SUITE(gil_io_tests)
 
@@ -30,6 +31,7 @@ BOOST_AUTO_TEST_CASE(non_bit_aligned_image_test){
     {rgb8_image_t img;
 read_image(bmp_filename, img, bmp_tag());
 #ifdef BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
+fs::create_directories(fs::path(bmp_out));
 write_view(bmp_out + "all_formats_test.bmp", view(img), bmp_tag());
 #endif // BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
 }
@@ -38,6 +40,7 @@ write_view(bmp_out + "all_formats_test.bmp", view(img), bmp_tag());
   rgb8_image_t img;
   read_image(jpeg_filename, img, jpeg_tag());
 #ifdef BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
+  fs::create_directories(fs::path(jpeg_out));
   write_view(jpeg_out + "all_formats_test.jpg", view(img), jpeg_tag());
 #endif // BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
 }
@@ -46,6 +49,7 @@ write_view(bmp_out + "all_formats_test.bmp", view(img), bmp_tag());
   rgba8_image_t img;
   read_image(png_filename, img, png_tag());
 #ifdef BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
+  fs::create_directories(fs::path(png_out));
   write_view(png_out + "all_formats_test.png", view(img), png_tag());
 #endif // BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
 }
@@ -54,6 +58,7 @@ write_view(bmp_out + "all_formats_test.bmp", view(img), bmp_tag());
   rgb8_image_t img;
   read_image(pnm_filename, img, pnm_tag());
 #ifdef BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
+  fs::create_directories(fs::path(pnm_out));
   write_view(pnm_out + "all_formats_test.pnm", view(img), pnm_tag());
 #endif // BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
 }
@@ -62,6 +67,7 @@ write_view(bmp_out + "all_formats_test.bmp", view(img), bmp_tag());
   rgb8_image_t img;
   read_image(targa_filename, img, targa_tag());
 #ifdef BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
+  fs::create_directories(fs::path(targa_out));
   write_view(targa_out + "all_formats_test.tga", view(img), targa_tag());
 #endif // BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
 }
@@ -70,6 +76,7 @@ write_view(bmp_out + "all_formats_test.bmp", view(img), bmp_tag());
   rgba8_image_t img;
   read_image(tiff_filename, img, tiff_tag());
 #ifdef BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
+  fs::create_directories(fs::path(tiff_out));
   write_view(tiff_out + "all_formats_test.tif", view(img), tiff_tag());
 #endif // BOOST_GIL_IO_TEST_ALLOW_WRITING_IMAGES
 }
