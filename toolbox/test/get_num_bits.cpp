boostorg/gil

Toolbox tests and documentation.
[SVN r82768]

toolbox/test/get_num_bits.cpp
@@ -1,0 +1,25 @@
+#include <boost/test/unit_test.hpp>
+
+#include <boost/type_traits/is_same.hpp>
+
+#include <boost/gil/gil_all.hpp>
+#include <boost/gil/extension/toolbox/metafunctions/channel_type.hpp>
+#include <boost/gil/extension/toolbox/metafunctions/get_num_bits.hpp>
+
+using namespace boost;
+using namespace gil;
+
+BOOST_AUTO_TEST_SUITE( get_num_bits_test )
+
+BOOST_AUTO_TEST_CASE( get_num_bits_test )
+{
+    typedef bit_aligned_image4_type<4, 4, 4, 4, rgb_layout_t>::type image_t;
+
+    typedef channel_type< image_t::view_t::reference >::type channel_t;
+    BOOST_STATIC_ASSERT( get_num_bits< channel_t >::value == 4 );
+
+    typedef channel_type< image_t::const_view_t::reference >::type const_channel_t;
+    BOOST_STATIC_ASSERT( get_num_bits< const_channel_t >::value == 4 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()

sdebionne/gil-reformated

toolbox/test/get_num_bits.cpp
@@ -1,0 +1,24 @@
+#include <boost/test/unit_test.hpp>
+
+#include <boost/type_traits/is_same.hpp>
+
+#include <boost/gil/extension/toolbox/metafunctions/channel_type.hpp>
+#include <boost/gil/extension/toolbox/metafunctions/get_num_bits.hpp>
+#include <boost/gil/gil_all.hpp>
+
+using namespace boost;
+using namespace gil;
+
+BOOST_AUTO_TEST_SUITE(get_num_bits_test)
+
+BOOST_AUTO_TEST_CASE(get_num_bits_test) {
+  typedef bit_aligned_image4_type<4, 4, 4, 4, rgb_layout_t>::type image_t;
+
+  typedef channel_type<image_t::view_t::reference>::type channel_t;
+  BOOST_STATIC_ASSERT(get_num_bits<channel_t>::value == 4);
+
+  typedef channel_type<image_t::const_view_t::reference>::type const_channel_t;
+  BOOST_STATIC_ASSERT(get_num_bits<const_channel_t>::value == 4);
+}
+
+BOOST_AUTO_TEST_SUITE_END()