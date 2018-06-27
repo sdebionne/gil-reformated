boostorg/gil

fixed a merging issue.
[SVN r84050]

include/boost/gil/image.hpp
@@ -152,11 +152,34 @@ public:
         swap( _original_dims, img._original_dims );
     }
 
-    void recreate(const point_t& dims, std::size_t alignment=0, const Alloc alloc_in = Alloc()) {
-        image tmp(dims, alignment, alloc_in);
-        swap(tmp);
-    }
+    void recreate(const point_t& dims, std::size_t alignment=0, const Alloc alloc_in = Alloc())
+    {
+        if( dims == _view.dimensions() && _align_in_bytes == alignment )
+        {
+            return;
+        }
 
+        std::size_t original_size = total_allocated_size_in_bytes( _original_dims );
+        std::size_t new_size = total_allocated_size_in_bytes( dims );
+
+        if( original_size >= new_size )
+        {
+            destruct_pixels( _view );
+
+            _view = view_t( dims
+                          , typename view_t::locator( typename view_t::x_iterator( _memory )
+                                                    , get_row_size_in_memunits( dims.x )
+                                                    )
+                          );
+
+            default_construct_pixels( _view );
+        }
+        else
+        {
+            image tmp(dims, alignment, alloc_in);
+            swap(tmp);
+        }
+    }
     void recreate(x_coord_t width, y_coord_t height, std::size_t alignment=0, const Alloc alloc_in = Alloc()) {
         recreate(point_t(width,height),alignment,alloc_in);
     }

sdebionne/gil-reformated

include/boost/gil/image.hpp
@@ -159,10 +159,26 @@ public:
 
   void recreate(const point_t &dims, std::size_t alignment = 0,
                 const Alloc alloc_in = Alloc()) {
-    image tmp(dims, alignment, alloc_in);
-    swap(tmp);
-  }
+    if (dims == _view.dimensions() && _align_in_bytes == alignment) {
+      return;
+    }
 
+    std::size_t original_size = total_allocated_size_in_bytes(_original_dims);
+    std::size_t new_size = total_allocated_size_in_bytes(dims);
+
+    if (original_size >= new_size) {
+      destruct_pixels(_view);
+
+      _view = view_t(
+          dims, typename view_t::locator(typename view_t::x_iterator(_memory),
+                                         get_row_size_in_memunits(dims.x)));
+
+      default_construct_pixels(_view);
+    } else {
+      image tmp(dims, alignment, alloc_in);
+      swap(tmp);
+    }
+  }
   void recreate(x_coord_t width, y_coord_t height, std::size_t alignment = 0,
                 const Alloc alloc_in = Alloc()) {
     recreate(point_t(width, height), alignment, alloc_in);