c6bdc3d980ae91829fddb23d4a027837cba0eb1c
Mateusz Loskot <mateusz@loskot.net>
2018-12-12 17:58:57 +0100

    Fix conflicting definitions from io/dynamic_io_new.hpp and toolbox/dynamic_images.hpp (#185)
    The problem appears to be due to the same definitions copied from one
    part of the library to the other.
    The definitions have been shuffled to fix the compilation,
    but purely based on searching the code for what is used where,
    thus without confidence where those belong by author's intention.
    
    Fixes #183

--- include/boost/gil/extension/io/tiff/detail/write.hpp
+++ include/boost/gil/extension/io/tiff/detail/write.hpp
@@ -14,6 +14,7 @@
 
 #include <boost/gil/io/base.hpp>
 #include <boost/gil/io/device.hpp>
+#include <boost/gil/io/dynamic_io_new.hpp>
 #include <boost/gil/premultiply.hpp>
 
 #include <boost/static_assert.hpp>
