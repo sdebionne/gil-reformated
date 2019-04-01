8ec689be4e05a1afdbdcdee5a1dc1d52a958a138
Mateusz Łoskot <mateusz@loskot.net>
2019-04-01 17:19:22 +0100

    Value-initialize channel and pixel value members (#273)
    Add tests to ensure member values of channel and pixel classes are
    determined, zero-initialized.
    Fixes #270
    
    Tidy up formatting.

--- include/boost/gil/channel.hpp
+++ include/boost/gil/channel.hpp
@@ -193,16 +193,16 @@ BOOST_PRAGMA_MESSAGE("CAUTION: Unaligned access tolerated on little-endian may "
     static value_type min_value() { return MinVal::apply(); }
     static value_type max_value() { return MaxVal::apply(); }
 
-    scoped_channel_value() {}
-    scoped_channel_value(const scoped_channel_value &c) : _value(c._value) {}
-    scoped_channel_value(BaseChannelValue val) : _value(val) {}
+    scoped_channel_value() = default;
+    scoped_channel_value(const scoped_channel_value &c) : value_(c.value_) {}
+    scoped_channel_value(BaseChannelValue val) : value_(val) {}
 
     scoped_channel_value &operator++() {
-      ++_value;
+      ++value_;
       return *this;
     }
     scoped_channel_value &operator--() {
-      --_value;
+      --value_;
       return *this;
     }
 
@@ -218,30 +218,30 @@ BOOST_PRAGMA_MESSAGE("CAUTION: Unaligned access tolerated on little-endian may "
     }
 
     template <typename Scalar2> scoped_channel_value &operator+=(Scalar2 v) {
-      _value += v;
+      value_ += v;
       return *this;
     }
     template <typename Scalar2> scoped_channel_value &operator-=(Scalar2 v) {
-      _value -= v;
+      value_ -= v;
       return *this;
     }
     template <typename Scalar2> scoped_channel_value &operator*=(Scalar2 v) {
-      _value *= v;
+      value_ *= v;
       return *this;
     }
     template <typename Scalar2> scoped_channel_value &operator/=(Scalar2 v) {
-      _value /= v;
+      value_ /= v;
       return *this;
     }
 
     scoped_channel_value &operator=(BaseChannelValue v) {
-      _value = v;
+      value_ = v;
       return *this;
     }
-    operator BaseChannelValue() const { return _value; }
+    operator BaseChannelValue() const { return value_; }
 
   private:
-    BaseChannelValue _value;
+    BaseChannelValue value_{};
   };
 
   template <typename T> struct float_point_zero {
@@ -317,21 +317,22 @@ BOOST_PRAGMA_MESSAGE("CAUTION: Unaligned access tolerated on little-endian may "
     static value_type min_value() { return 0; }
     static value_type max_value() { return low_bits_mask_t<NumBits>::sig_bits; }
 
-    packed_channel_value() {}
+    packed_channel_value() = default;
     packed_channel_value(integer_t v) {
-      _value =
+      value_ =
           static_cast<integer_t>(v & low_bits_mask_t<NumBits>::sig_bits_fast);
     }
+
     template <typename Scalar> packed_channel_value(Scalar v) {
-      _value = packed_channel_value(static_cast<integer_t>(v));
+      value_ = packed_channel_value(static_cast<integer_t>(v));
     }
 
     static unsigned int num_bits() { return NumBits; }
 
-    operator integer_t() const { return _value; }
+    operator integer_t() const { return value_; }
 
   private:
-    integer_t _value;
+    integer_t value_{};
   };
 
   namespace detail {
