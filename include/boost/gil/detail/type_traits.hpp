b666ef6098cd6cda69c6ad127e4dd292c63308f6
Mateusz Łoskot <mateusz@loskot.net>
2019-04-20 14:33:50 +0200

    GCC 4.8/4.9 define std::is_trivially_destructible (#292)

--- include/boost/gil/detail/type_traits.hpp
+++ include/boost/gil/detail/type_traits.hpp
@@ -16,7 +16,7 @@ namespace boost {
 namespace gil {
 namespace detail {
 
-#if defined(BOOST_LIBSTDCXX_VERSION) && BOOST_LIBSTDCXX_VERSION < 50000
+#if defined(BOOST_LIBSTDCXX_VERSION) && BOOST_LIBSTDCXX_VERSION < 50100
 
 template <class T>
 struct is_trivially_default_constructible
@@ -24,18 +24,13 @@ struct is_trivially_default_constructible
           bool, std::is_default_constructible<T>::value &&
                     std::has_trivial_default_constructor<T>::value> {};
 
-template <class T>
-struct is_trivially_destructible
-    : std::integral_constant<bool, std::is_destructible<T>::value &&
-                                       std::has_trivial_destructor<T>::value> {
-};
-
 #else
 
 using std::is_trivially_default_constructible;
-using std::is_trivially_destructible;
 
 #endif
+
+using std::is_trivially_destructible;
 
 } // namespace detail
 } // namespace gil
