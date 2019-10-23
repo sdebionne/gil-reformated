ee01e6244661dc72ef37d779ffcc454331134e97
Mateusz Łoskot <mateusz@loskot.net>
2019-10-23 21:49:14 +0200

    Remove threshold_optimal_value::triangle enumerator as not implemented
    This is part of planned work that has not been completed yet, see
    @lpranam comments https://lists.boost.org/boost-gil/2019/10/0315.php

--- include/boost/gil/image_processing/threshold.hpp
+++ include/boost/gil/image_processing/threshold.hpp
@@ -67,8 +67,7 @@ enum class threshold_direction {
 /// \ingroup ImageProcessing
 /// \brief Method of optimal threshold value calculation.
 enum class threshold_optimal_value {
-  otsu,    ///< \todo TODO
-  triangle ///< \todo TODO
+  otsu ///< \todo TODO
 };
 
 /// \ingroup ImageProcessing
